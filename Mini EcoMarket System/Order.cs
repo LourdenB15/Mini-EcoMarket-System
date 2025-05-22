4b152b4bb892bc36916f69cae69cff0301af2d34
LourdenB15 <103452827+LourdenB15@users.noreply.github.com>
2025-05-22 12:46:03 +0800

    Update Order.cs

--- Mini EcoMarket System/Order.cs
+++ Mini EcoMarket System/Order.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace Mini_EcoMarket_System
 {
-public class Order
-{
-    public string ProductName { get; set; }
-    public int Quantity { get; set; }
-    public double TotalPrice { get; set; }
-
-    public Order(string productName, int quantity, double totalPrice)
+    public class Order
     {
-        ProductName = productName;
-        Quantity = quantity;
-        TotalPrice = totalPrice;
-    }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
 
-    public override string ToString()
-    {
-        return $"{Quantity} x {ProductName} - Total: ${TotalPrice}";
+        public Order(string productName, int quantity, double totalPrice)
+        {
+            ProductName = productName;
+            Quantity = quantity;
+            TotalPrice = totalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantity} x {ProductName} - Total: ${TotalPrice}";
+        }
     }
-}
 }
