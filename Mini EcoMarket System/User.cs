437e1088c671ae9ff5c6acb9a41b1650943421b6
LourdenB15 <103452827+LourdenB15@users.noreply.github.com>
2025-05-22 12:54:58 +0800

    Update User.cs

--- Mini EcoMarket System/User.cs
+++ Mini EcoMarket System/User.cs
@@ -6,11 +6,13 @@ namespace Mini_EcoMarket_System
     {
         public string Username { get; set; }
         public string Email { get; set; }
+        
         public User(string username, string email)
         {
             Username = username;
             Email = email;
         }
+        
         public abstract void DisplayInfo();
         public abstract string GetRole();
     }
