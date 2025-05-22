26ac905a7f03008c836694ffa74584f3b4ec5a46
LourdenB15 <103452827+LourdenB15@users.noreply.github.com>
2025-05-22 12:45:07 +0800

    Update Customer.cs

--- Mini EcoMarket System/Customer.cs
+++ Mini EcoMarket System/Customer.cs
@@ -9,9 +9,8 @@ namespace Mini_EcoMarket_System
     public class Customer : User
     {
         public List<Order> OrderHistory { get; private set; } = new List<Order>();
-
-        public Customer(string username, string email) : base(username, email) { }
-
+        public Customer(string username, string email) : base(username, email) {}
+        
         public void PlaceOrder(Product product, int quantity)
         {
             product.Stock -= quantity;
@@ -19,7 +18,7 @@ namespace Mini_EcoMarket_System
             OrderHistory.Add(order);
             Console.WriteLine("Order placed successfully.");
         }
-
+        
         public override void DisplayInfo()
         {
             Console.WriteLine($"Customer: {Username}, Email: {Email}, Orders Made: {OrderHistory.Count}");
